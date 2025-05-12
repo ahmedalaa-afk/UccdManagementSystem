48f70157b4a46c022db39e9b5fd72e5ee7a4803e
Ahmed Alaa <ahmed.alaaeldine21@gmail.com>
2025-05-12 15:49:51 +0300

    Add getAllStudents method to StudentController and improve import/export methods with better formatting and error handling

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -5,25 +5,35 @@ namespace App\Http\Controllers\Admin;
 use App\Exports\StudentExport;
 use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\RetriveStudentsResource;
 use App\Imports\StudentImport;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
 class StudentController extends Controller
 {
-    public function import(Request $request){
-        if($request->hasFile('file')){
+    public function import(Request $request)
+    {
+        if ($request->hasFile('file')) {
             $request->validate([
                 'file' => 'required|file|mimes:xlsx'
             ]);
             $file = $request->file('file');
             Excel::import(new StudentImport, $file);
-            return ApiResponse::sendResponse('Students imported successfully.', [],true);
+            return ApiResponse::sendResponse('Students imported successfully.', [], true);
         }
-        return ApiResponse::sendResponse('somthing went wrong with file pleas try again.',[],false);
+        return ApiResponse::sendResponse('somthing went wrong with file pleas try again.', [], false);
     }
 
-    public function export(){
+    public function export()
+    {
         return Excel::download(new StudentExport, 'students.xlsx');
+    }
+
+    public function getAllStudents()
+    {
+        $students = User::where('role', 'student')->get();
+        return ApiResponse::sendResponse('Students retrieved successfully.', RetriveStudentsResource::collection($students), true);
     }
 }
